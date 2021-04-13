ea744d1ee62b465e71e2d5f206c7baf486ea458f
Salih Saygi <salih.saygie@gmail.com>
2021-04-12 22:42:53 -0700

    Spotify fix

--- website/front/src/components/UI/Pages/SpotifyHub/main.js
+++ website/front/src/components/UI/Pages/SpotifyHub/main.js
@@ -1,7 +1,7 @@
 import SpotifyAudio from './index'
 import SpotifyLogin from '../../Pieces/SpotifyLogin'
 
-const queryCode = new URLSearchParams(window.location.search).get("queryCode")
+const queryCode = new URLSearchParams(window.location.search).get("code")
 
 function SpotifyHub() {
     return queryCode ? <SpotifyAudio queryCode={queryCode}/> : <SpotifyLogin/>
