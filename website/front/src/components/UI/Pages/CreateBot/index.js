035e9e78711966e298e01a28d36911aa652f519e
aaronTheZinc <aaronmarsh755@gmail.com>
2021-04-14 22:21:26 -0400

    new ui

--- website/front/src/components/UI/Pages/CreateBot/index.js
+++ website/front/src/components/UI/Pages/CreateBot/index.js
@@ -2,25 +2,30 @@ import React, { useContext, useReducer } from "react";
 import { Bot as NewBot } from "../../../Classes/classes";
 import { PrimaryButton as Button } from "../../Pieces/button/index";
 import { PrimaryInput as Input } from "../../Pieces/Input/index";
+import {appState} from '../../../State/Context'
 import Reducers from "../../../State/reducers";
 import "./index.css";
+
+const buttonTheme = {
+  textColor: "",
+  backgroundColor: "",
+  width: "600px",
+  height: "100px",
+};
+const inputTheme = {
+  width: "15vw",
+  height: "80px",
+  backgroundColor: "#151A21",
+  color: "#FD6868",
+};
+
 export default () => {
+  const context = useContext(appState);
+  console.log(context.state)
   const {
     newBot: { initialAppState, setNewBotReducer },
   } = Reducers;
   const [state, dispatch] = useReducer(setNewBotReducer, initialAppState);
-  const buttonTheme = {
-    textColor: "",
-    backgroundColor: "",
-    width: "300px",
-    height: "100px",
-  };
-  const inputTheme = {
-    width: "10vw",
-    height: "80px",
-    backgroundColor: "#151A21",
-    color: "#FD6868",
-  };
 
   const Bot = new NewBot();
   return (
@@ -44,23 +49,42 @@ export default () => {
         <div className="new-bot-form-container">
           <section className="new-bot-form">
             <section className="new-bot-input-container">
-              <div style={{ display: "flex", marginTop:'10%'  }}>
-                <section style={{ marginLeft: '25%' ,float: "left", justifySelf: 'center' }}>
-                  <Input
-                    theme={inputTheme}
-                    onChange={() => console.log("input test")}
-                    placeholder="Bot Profile"
-                  />
-                </section>
-                <section style={{ float: "right", marginLeft: '20%' }}>
-                  <Input
-                    theme={inputTheme}
-                    onChange={() => console.log("input test")}
-                    placeholder="Bot Profile"
-                  />
-                </section>
+              <div style={{ marginLeft: "15%" }}>
+                <div style={{ display: "flex" }}>
+                  <section style={{ justifySelf: "center", marginTop: "10vh" }}>
+                    <Input
+                      theme={inputTheme}
+                      onChange={() => dispatch({ type: "name", value: "" })}
+                      placeholder="Bot Profile Name"
+                    />
+                  </section>
+                  <section style={{ marginLeft: "10%", marginTop: "10vh" }}>
+                    <Input
+                      theme={inputTheme}
+                      onChange={() => console.log("input test")}
+                      placeholder="Bot Profile Image URL"
+                    />
+                  </section>
+                </div>
+                <div style={{ display: "flex", marginTop: "10%" }}>
+                  <section style={{ justifySelf: "center" }}>
+                    <Input
+                      theme={inputTheme}
+                      onChange={() => console.log("input test")}
+                      placeholder="Token"
+                    />
+                  </section>
+                  <section style={{ marginLeft: "10%" }}>
+                    <Input
+                      theme={inputTheme}
+                      onChange={() => console.log("input test")}
+                      placeholder="Refresh Token"
+                    />
+                  </section>
+                </div>
               </div>
             </section>
+
             <section className="create-bot-button">
               <Button
                 theme={buttonTheme}
